51d91f0377c6c8e9ce95caf2726335a163776d5c
Edgar José Naranjo González <edgarnaranjoglez@gmail.com>
2024-02-16 21:20:20 +0000

    Cambios txt

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("Edgar se ha pasado por aquí");
+        System.out.println("Cambios everywhere");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
